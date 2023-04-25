1e92ad70a91abad32628b4e291cfc8e22e95d641
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-25 10:39:37 +0100

    exports both functions

--- Phase-One/add.js
+++ Phase-One/add.js
@@ -1,5 +1,9 @@
+const multiply = require("./multiply");
+
 const add = (a, b) => {
   return a + b;
 };
 
 console.log(add(2, 3));
+
+module.exports = add;
---
+++ Phase-One/multiply.js
@@ -0,0 +1,7 @@
+const multiply = (a, b) => {
+  return a * b;
+};
+
+console.log(multiply(2, 3));
+
+module.exports = multiply;
