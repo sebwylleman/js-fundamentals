3b9859b8525946b80324d5baf7f9ea4ba63e625e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-26 15:50:19 +0100

    using filter

--- Phase-One/arrayOperations.js
+++ Phase-One/arrayOperations.js
@@ -7,3 +7,19 @@ const checklength = (string) => {
 
 console.log(checklength('AAAA#####AAAA#87@768767382672'));
 console.log(checklength('1763687364'));
+
+// Using the filter function
+const numbers = [
+  '1763687364',
+  '4763687363',
+  '7867867862',
+  'AAAA#####AAAA#87@768767382672',
+  '4763687363',
+  '4763687363',
+];
+
+const isPhoneNumber = (n) => {
+  return n.length <= 10;
+};
+
+console.log(numbers.filter(isPhoneNumber));
