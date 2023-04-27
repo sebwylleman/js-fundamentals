e98a60ef801d8aa7a882a6450d76ee070ce7b51b
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-27 08:59:52 +0100

    passes fizz test

--- Phase-Two/TestProject/fizzbuzz.test.js
+++ Phase-Two/TestProject/fizzbuzz.test.js
@@ -4,4 +4,7 @@ describe('fizzbuzz', () => {
   it('prints fizzbuzz for multiples of 3 and 5', () => {
     expect(fizzBuzz(15)).toBe('fizzbuzz');
   });
+  it('prints fizz for mutiples of 3', () => {
+    expect(fizzBuzz(3)).toBe('fizz');
+  });
 });
