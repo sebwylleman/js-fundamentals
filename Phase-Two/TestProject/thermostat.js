b5c9696c55b254b11bd4f275a48a980f1715d244
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-27 14:13:36 +0100

    initial temperature functionality is being tested

---
+++ Phase-Two/TestProject/thermostat.js
@@ -0,0 +1,5 @@
+class Thermostat {
+  constructor() {}
+}
+
+module.exports = Thermostat;
---
+++ Phase-Two/TestProject/thermostat.test.js
@@ -0,0 +1,8 @@
+const Thermostat = require('./thermostat');
+const myThermostat = new Thermostat();
+
+describe('Thermostat', () => {
+  it('returns an initial temperature of 20', () => {
+    expect(myThermostat.getTemperature).toEqual(20);
+  });
+});
