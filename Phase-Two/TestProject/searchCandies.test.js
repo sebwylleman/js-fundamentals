09e50f2a4ebde333be6e78459524817ff0b724e3
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-27 10:24:01 +0100

    typo in the last test. Should all work now

--- Phase-Two/TestProject/searchCandies.test.js
+++ Phase-Two/TestProject/searchCandies.test.js
@@ -4,13 +4,13 @@ describe('searchCandies', () => {
   it("returns candies with prefix 'Ma' and maximum price: 5", () => {
     expect(searchCandies('Ma', 5)).toEqual(['Mars', 'Maltesers']);
   });
-  xit("returns candies with prefix 'P' and maximum price: 10", () => {
-    expect(searchCandies('P', 5)).toEqual(['Pastilla']);
+  it("returns candies with prefix 'P' and maximum price: 10", () => {
+    expect(searchCandies('P', 5)).toEqual(['Pastila']);
   });
-  xit('returns candies with prefix Q and maximum price: 10', () => {
+  it('returns candies with prefix Q and maximum price: 10', () => {
     expect(searchCandies('Q', 10)).toEqual([]);
   });
-  xit("returns candies with prefix 'Ma' and maximum price: -2", () => {
+  it("returns candies with prefix 'Ma' and maximum price: -2", () => {
     expect(searchCandies('Ma', -2)).toEqual([]);
   });
 });
